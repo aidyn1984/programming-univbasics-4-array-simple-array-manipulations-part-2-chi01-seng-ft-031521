5499d0f8b6729f9ef65e36069da62bd665a90535
Aiden Seitz <csa.aiden@gmail.com>
2021-02-23 19:34:55 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(parameter1, parameter2)
+  parameter1.concat(parameter2)
+end
+
+
+def using_insert(array,item)
+  array.insert(4, item)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array,string)
+  array.delete(string)
+end
+
+def using_delete_at(array,index)
+  array.delete_at(index)
+end
